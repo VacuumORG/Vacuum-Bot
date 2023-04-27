1b8919f320cc0bedeafaccc0bb6391524d2df3ab
oblivion <oblivionccyber@gmail.com>
2023-04-27 07:52:47 -0300

    rename returb func

--- jobs/Sites.py
+++ jobs/Sites.py
@@ -7,7 +7,7 @@ class Sites:
     def __init__(self) -> None:
         self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'
         self.headers = {'User-Agent': self.user_agent}
-    
+
 
     def linkedin_jobs(self,search: str, cd: bool) -> List[dict]:
         cd = '?f_AL=true&' if cd else ''
@@ -23,7 +23,7 @@ class Sites:
 
         return jobs
 
-    def nerdin_vagas(self,search: str) -> List[dict]:
+    def nerdin_jobs(self,search: str) -> List[dict]:
         url =f'https://www.nerdin.com.br/func/FVagaListar.php?F=HO&NomeEspeciPalavraChave={search}&PermiteTrabalhoRemoto=0'
         response = requests.get(url=url, headers=self.headers)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -31,9 +31,9 @@ class Sites:
         job_links = ['https://www.nerdin.com.br/' + link.get('href') for link in soup.find_all('a', href=True) if link.get('href').startswith('vaga/')][:3]
         job_name = soup.select('span:nth-child(1) b')[:3]
 
-        job_list = list(map(lambda x: {'Job': x[1].text, 'Apply': x[0]}, zip(job_links, job_name)))
+        jobs = list(map(lambda x: {'Job': x[1].text, 'Apply': x[0]}, zip(job_links, job_name)))
 
-        return job_list
+        return jobs
 
 
     def thor_jobs(self,search: str)-> List[dict]:
@@ -48,9 +48,9 @@ class Sites:
         soup = BeautifulSoup(response.text,'html.parser')
         jobs = soup.select('.cell-list')[:3]
 
-        vagas = [{'Job': job.find_all('h3')[0].get_text(),
+        jobs = [{'Job': job.find_all('h3')[0].get_text(),
           'Apply': "https://programathor.com.br"+ job.select('a')[0].attrs['href'],
           'Techs': [tech.get_text() for tech in job.find_all('span', {'class': 'tag-list background-gray'})]} for job in jobs]
 
-        return vagas
+        return jobs
 
