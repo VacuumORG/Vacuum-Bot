12e7015b212701a36dd9e1b163c4a51d0a7a6a23
masachetti <50852444+masachetti@users.noreply.github.com>
2023-05-02 08:50:33 -0400

    Create admin Cog with reload and sync commands

---
+++ cogs/admin.py
@@ -0,0 +1,37 @@
+from discord.ext import commands
+from discord.ext.commands import Context
+
+
+class Admin(commands.Cog):
+    """Admin-only commands that make the bot dynamic."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name='reload')
+    async def _reload(self, ctx: Context, *, module: str):
+        """Reloads a module."""
+        await ctx.send(f'Try to reload {module}')
+        try:
+            await self.bot.reload_extension('cogs.' + module)
+        except Exception as e:
+            await ctx.send("Cannot reload the module. Exception raised:")
+            await ctx.send('{}: {}'.format(type(e).__name__, e))
+        else:
+            await ctx.send('\N{OK HAND SIGN}')
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.is_owner()
+    async def sync(self, ctx: Context) -> None:
+        """Sync the app commands tree with Discord."""
+        try:
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+            synced_str = ",".join([sync_command.name for sync_command in synced])
+            await ctx.send(f"Synced {synced_str} commands!")
+        except Exception as e:
+            await ctx.send(f"Cannot sync commands. Exception:\n{e}")
+
+
+async def setup(bot):
+    await bot.add_cog(Admin(bot))
