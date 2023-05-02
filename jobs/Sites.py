fcd77edfb9f19d3d00a55fa7dc9ec9bd68644a40
masachetti <50852444+masachetti@users.noreply.github.com>
2023-05-02 17:46:47 -0400

    Remove list slicing on Thor scrapper and prevent exceptions for broken jobs.

--- jobs/Sites.py
+++ jobs/Sites.py
@@ -3,6 +3,14 @@ from bs4 import BeautifulSoup
 from typing import List
 
 
+def check_if_thor_job_is_expired(job_soup):
+    return 'opacity-60p' in job_soup.attrs['class']
+
+
+def check_if_thor_job_is_broken(job_soup):
+    return 'min-height-180' in job_soup.attrs['class']
+
+
 class Sites:
     def __init__(self) -> None:
         self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0'
@@ -39,8 +47,7 @@ class Sites:
 
         return jobs
 
-
-    def thor_jobs(self,search: str)-> List[dict]:
+    def thor_jobs(self, search: str) -> List[dict]:
         """
         Args:
             search (str): this string can only receive 3 parameters which are 'Júnior'/'Pleno'/'Sênior'.
@@ -48,12 +55,15 @@ class Sites:
             List[dict]: returns a dictionary list with the keys 'Job: str'/'Apply: str'/'Techs: list of str'.
         """
         search_junior = f'https://programathor.com.br/jobs-city/remoto?expertise={search}'
-        response = requests.get(search_junior,headers=self.headers)
-        soup = BeautifulSoup(response.text,'html.parser')
-        jobs = soup.select('.cell-list')[:3]
+        response = requests.get(search_junior, headers=self.headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        jobs = soup.select('.cell-list')
+
+        jobs = [job for job in jobs if not check_if_thor_job_is_broken(job) and not check_if_thor_job_is_expired(job)]
 
         jobs = [{'Job': job.find_all('h3')[0].get_text(),
-          'Apply': "https://programathor.com.br"+ job.select('a')[0].attrs['href'],
-          'Techs': [tech.get_text() for tech in job.find_all('span', {'class': 'tag-list background-gray'})]} for job in jobs]
+                 'Apply': "https://programathor.com.br" + job.select('a')[0].attrs['href'],
+                 'Techs': [tech.get_text() for tech in job.find_all('span', {'class': 'tag-list background-gray'})]} for
+                job in jobs]
 
         return jobs
