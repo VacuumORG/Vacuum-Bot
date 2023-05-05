72433a4c563ca5cb3c5a33cfd1187e1bf94fb3e5
masachetti <50852444+masachetti@users.noreply.github.com>
2023-05-05 15:29:52 -0400

    Remove tools.py (unnecessary code)

--- tools.py
+++
@@ -1,7 +0,0 @@
-def chunks(lst, chunk_size):
-    ret = []
-    temp = list(lst)
-    while temp:
-        chunk, temp = temp[:chunk_size], temp[chunk_size:]
-        ret.append(chunk)
-    return ret
