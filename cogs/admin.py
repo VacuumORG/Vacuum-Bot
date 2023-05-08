54ce1d4c0a16531a0cd5fb6de102f4be0364464c
masachetti <50852444+masachetti@users.noreply.github.com>
2023-05-08 15:50:11 -0400

    Remove get_helper from admin Cog

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,13 +8,6 @@ class Admin(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    def get_helper(self):
-        help_text = """• /reload [cog_name] - Recarrega o cog especificado.
-        • /sync - Sincroniza os App Commands do bot com o Discord.
-        """
-        group_name = "Administrador"
-        return [group_name, help_text]
-
     @commands.command(name='reload')
     async def _reload(self, ctx: Context, *, module: str):
         """Reloads a module."""
