051cdb65976de3a49f4b0e3b281931be4b3a7bf5
masachetti <50852444+masachetti@users.noreply.github.com>
2023-05-08 15:00:23 -0400

    Set up logging

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import asyncio
+import logging.handlers
 from os import getenv
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+# Setting log
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='log/discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Setting env variables
 load_dotenv()
 token = getenv('TOKEN')
 guild_id = getenv('GUILD_ID')
 
+# Setting bot
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
@@ -20,10 +39,6 @@ extensions = [
     'cogs.vagas',
     'cogs.utils'
 ]
-
-
-def format_dict_list(dict_list):
-    return "\n".join(f"{key}: {value}" for d in dict_list for key, value in d.items())
 
 
 @bot.event
@@ -50,4 +65,4 @@ bot.setup_hook = setup_hook
 
 if __name__ == "__main__":
     asyncio.run(setup_extensions())
-    bot.run(token)
+    bot.run(token, log_handler=None)
