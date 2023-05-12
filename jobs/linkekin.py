8ffdbff7da449e9c739be898335be5cfbf071038
masachetti <50852444+masachetti@users.noreply.github.com>
2023-05-11 22:22:44 -0400

    Fix LinkedIn job urls

--- jobs/linkekin.py
+++ jobs/linkekin.py
@@ -9,7 +9,7 @@ from jobs.utils import sanitize_job_title
 async def scrap_linkedin_jobs(search: str, cd: bool) -> List[dict | Exception]:
     search = f"TI {search}"
     cd = '?f_AL=true&' if cd else ''
-    url = f'https://www.linkedin.com/jobs/search/?f_WT=2&geoId=106057199&keywords={search}&{cd}location=Brazil&position=1&pageNum=0&f_TPR=r604800'
+    url = f'https://www.linkedin.com/jobs/search/?f_WT=2&geoId=106057199&keywords={search}&{cd}location=Brazil&position=1&pageNum=0&f_TPR=r2592000'
 
     resp_content = await fetch_urls([url])
     if isinstance(resp_content[url], Exception):
@@ -19,8 +19,13 @@ async def scrap_linkedin_jobs(search: str, cd: bool) -> List[dict | Exception]:
     job_elements = soup.select_one('ul.jobs-search__results-list').select('li')
     jobs = []
     for job_element in job_elements:
-        job_a = job_element.select_one('div a')
-        job_link = job_a.get('href')
-        job_title = job_a.select_one('span').text
-        jobs.append({'Job': sanitize_job_title(job_title), 'Apply': job_link})
+        job_div = job_element.select_one('div')
+        if not job_div.has_attr('data-entity-urn'):
+            job_a = job_element.select_one('a')
+            job_id = job_a['data-entity-urn'].split(":")[3]
+            job_title = job_a.select_one('div h3').text
+        else:
+            job_id = job_div['data-entity-urn'].split(":")[3]
+            job_title = job_div.select_one('a span').text
+        jobs.append({'Job': sanitize_job_title(job_title), 'Apply': f"https://www.linkedin.com/jobs/view/{job_id}"})
     return jobs
