e51edaca7df65b1ebffbb71d2f40dd6ce0852f7e
masachetti <50852444+masachetti@users.noreply.github.com>
2023-06-14 22:26:59 -0400

    feat(ui): create view menu with buttons

---
+++ utils/menu_buttons.py
@@ -0,0 +1,73 @@
+"""
+    -
+
+
+"""
+import math
+from typing import TypedDict, List, Callable, Optional, Awaitable
+
+from discord import Interaction
+from discord.ui import View, Button
+
+ButtonArgs = TypedDict('ButtonArgs',
+                       {'label': str, 'emoji': str},
+                       total=False)
+
+
+def slice_list(arr, size):
+    return [[x for x in arr[i * size:(i + 1) * size]] for i in range(math.ceil(len(arr) / size))]
+
+
+class MenuButtons(View):
+    def __init__(self, buttons: List[ButtonArgs], max_page_buttons=25,
+                 callback: Optional[Callable[[int, Interaction], Awaitable]] = None,
+                 update_callback: Optional[Callable[[], Awaitable]] = None):
+        super().__init__()
+        self.buttons_args = buttons
+        self.max_page_buttons = min(max_page_buttons, 25) if max_page_buttons > 0 else 25
+        self.callback = callback
+        self.update_callback = update_callback
+        self._page = 0
+
+        self.pages = slice_list(self.buttons_args, self.max_page_buttons - 2)
+        self._paginated = len(self.pages) > 1
+
+        self.update_view()
+
+    async def __back_callback(self, interaction: Interaction):
+        await interaction.response.defer()
+        self._page = self._page - 1 if self._page else len(self.pages) - 1
+        self.update_view()
+        await self.update_callback()
+
+    async def __next_callback(self, interaction: Interaction):
+        await interaction.response.defer()
+        self._page = 0 if self._page + 1 == len(self.pages) else self._page + 1
+        self.update_view()
+        await self.update_callback()
+
+    def __create_bt_callback(self, bt_index):
+        async def __bt_callback(interaction: Interaction):
+            await interaction.response.defer()
+            if callable(self.callback):
+                await self.callback(bt_index, interaction)
+
+        return __bt_callback
+
+    def update_view(self):
+        self.clear_items()
+        if self._paginated:
+            back_bt = Button(emoji="◀")
+            back_bt.callback = self.__back_callback
+            self.add_item(back_bt)
+        for i, bt_args in enumerate(self.pages[self._page]):
+            bt = Button(**bt_args)
+            bt.callback = self.__create_bt_callback(i + self._page * (self.max_page_buttons - 2))
+            self.add_item(bt)
+        if self._paginated:
+            next_bt = Button(emoji="▶")
+            next_bt.callback = self.__next_callback
+            self.add_item(next_bt)
+
+
+a = MenuButtons(buttons=[{'label': 'sfsd'}])
---
+++ utils/view_menu.py
@@ -0,0 +1,33 @@
+from typing import TypeAlias, List
+
+from discord import Interaction, Embed
+
+from utils.menu_buttons import MenuButtons
+
+PageType: TypeAlias = tuple[str, Embed | List[Embed]]
+
+
+class ViewMenu:
+    def __init__(self, interaction: Interaction, pages: List[PageType]):
+        self.interaction = interaction
+        self.pages = pages
+        self._page = 0
+        self._actual_page: PageType = self.pages[0]
+        self._view = MenuButtons(buttons=[{'label': page[0]} for page in self.pages])
+        self._view.callback = self.__view_callback
+        self._view.update_callback = self.update
+
+    async def __view_callback(self, index, interaction):
+        self._page = index
+        self._actual_page = self.pages[index]
+        await self.update()
+
+    async def start(self):
+        message_args = {'embeds' if isinstance(self._actual_page[1], list) else 'embed': self._actual_page[1],
+                        'view': self._view}
+        await self.interaction.response.send_message(**message_args)
+
+    async def update(self):
+        message_args = {'embeds' if isinstance(self._actual_page[1], list) else 'embed': self._actual_page[1],
+                        'view': self._view}
+        await self.interaction.edit_original_response(**message_args)
