506800a09bc192134107b59f0aaa4c4924878b25
masachetti <50852444+masachetti@users.noreply.github.com>
2023-06-15 16:33:17 -0400

    style: rename 'commands' to 'comandos'

--- cogs/bot_commands.py
+++ cogs/bot_commands.py
@@ -9,7 +9,7 @@ class BotCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @discord.app_commands.command(name='commands')
+    @discord.app_commands.command(name='comandos')
     async def _commands(self, interaction: Interaction):
         hasHelper = lambda x: hasattr(x, 'get_helper') and callable(getattr(x, 'get_helper'))
         helpers = [cog.get_helper() for cog in self.bot.cogs.values() if hasHelper(cog)]
