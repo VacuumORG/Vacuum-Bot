365a944de00897b367ab832da685939cd632c664
masachetti <50852444+masachetti@users.noreply.github.com>
2023-06-19 17:23:12 -0400

    feat: add 'only owner allowed click' behavior to buttons menu

--- utils/view_menu.py
+++ utils/view_menu.py
@@ -8,20 +8,16 @@ PageType: TypeAlias = tuple[str, Embed | List[Embed]]
 
 
 class ViewMenu:
-    def __init__(self, interaction: Interaction, pages: List[PageType], all_can_click=False):
+    def __init__(self, interaction: Interaction, pages: List[PageType]):
         self.interaction = interaction
         self.pages = pages
         self._page = 0
         self._actual_page: PageType = self.pages[0]
-        self._view = MenuButtons(buttons=[{'label': page[0]} for page in self.pages])
+        self._view = MenuButtons(owner=interaction.user, buttons=[{'label': page[0]} for page in self.pages])
         self._view.callback = self.__view_callback
         self._view.update_callback = self.update
-        self.all_can_click = all_can_click
-        self._owner = self.interaction.user
 
-    async def __view_callback(self, index, interaction):
-        if not self.all_can_click and interaction.user != self._owner:
-            return
+    async def __view_callback(self, index, *args):
         self._page = index
         self._actual_page = self.pages[index]
         await self.update()
